f74456be95695f5bbf234513fe23a0eb0cd45a10
ut2zwj <edouard.gomezvaez-prestataire@ca-cib.com>
2021-12-01 15:22:44 +0100

    D1 : simplification de l'inégalité (niveau 4è ;)) et factorisation

--- src/main/java/domain/Day1.java
+++ src/main/java/domain/Day1.java
@@ -9,20 +9,19 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Day1 {
+
     public static List<Integer> readFile(String suffix) throws IOException {
-        Path path = Paths.get("src/main/resources/day"+suffix+".txt");
+        Path path = Paths.get("src/main/resources/day" + suffix + ".txt");
         Stream<String> lines = Files.lines(path);
         return lines.map(Integer::parseInt).collect(Collectors.toList());
     }
 
     public static int countSimpleIncreases(List<Integer> measures) {
-        int count = 0;
-        for (int i = 1; i < measures.size(); i++) {
-            if (measures.get(i) > measures.get(i-1)) {
-                count++;
-            }
-        }
-        return count;
+        return compareAtSteps(measures, 1);
+    }
+
+    public static int countCumulativeIncreases(List<Integer> measures) {
+        return compareAtSteps(measures, 3);
     }
 
     public static void main(String[] args) throws IOException {
@@ -30,11 +29,10 @@ public class Day1 {
         System.out.println(countCumulativeIncreases(readFile("1")));
     }
 
-    public static int countCumulativeIncreases(List<Integer> measures) {
+    private static int compareAtSteps(List<Integer> measures, int steps) {
         int count = 0;
-        for (int i = 3; i < measures.size(); i++) {
-            if (measures.get(i-2) + measures.get(i-1) + measures.get(i)
-                    > measures.get(i-3) + measures.get(i-2) + measures.get(i-1)) {
+        for (int i = steps; i < measures.size(); i++) {
+            if (measures.get(i) > measures.get(i - steps)) {
                 count++;
             }
         }
